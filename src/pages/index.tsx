0a235e29fe2c9a1f677c1919f1effc73ea0c747c
ldoeppen <Lawrence.doeppenschmidt@wpengine.com>
2021-09-15 15:00:03 -0500

    New code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Page() {
 
       <main className="content">
         <Hero
-          title="OK maybe later"
+          title="OK maybe"
           buttonText="Developer Docs"
           buttonURL="https://developers.wpengine.com/"
           button2Text="Headless on GitHub"
